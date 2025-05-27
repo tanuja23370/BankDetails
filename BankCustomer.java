dd460aa6308bc7f509306188fcd820a75fd609a0
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-27 10:15:46 -0300

    save

---
+++ BankCustomer.java
@@ -0,0 +1,24 @@
+package BankDetails;
+
+public class BankCustomer {
+    String custName;
+    int customerId;
+    String customerAddress;
+
+    public BankCustomer(String custName, int customerId, String customerAddress) {
+        this.custName = custName;
+        this.customerId = customerId;
+        this.customerAddress = customerAddress;
+    }
+
+    public void displayCustomer() {
+        System.out.println("Customer Name: " + custName);
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("Customer Address: " + customerAddress);
+    }
+
+    public static void main(String[] args) {
+        BankCustomer customer = new BankCustomer("Alice", 101, "123 Park Street");
+        customer.displayCustomer();
+    }
+}
